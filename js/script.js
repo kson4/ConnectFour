794f5d5f29e4990c82a9dd8149ab15d4f46bca2b
david <davidsinternetmail@gmail.com>
2022-08-09 20:19:19 -0700

    clicker

--- js/script.js
+++ js/script.js
@@ -9,23 +9,45 @@ function makeGrid(boardSize, numCells) {
         rows.setAttribute("id", row);
         container.appendChild(rows);
         for (let col = 0; col < numCells; col++) {
-            const cell = document.createElement("button");
+            const cell = document.createElement("div");
             cell.className = "cell";
-            cell.id = id++;
+            cell.id = "notclicked";
             cell.style.width = cellSize;
             cell.style.height = cellSize;
+            //cell.addEventListener("click", selection);
             rows.appendChild(cell);
         }
     }
 }
-
-const selection = document.getElementsByClassName("cell");
-function playGame() {
-    playerMove();
-}
-
-function playerMove() {
-    
-}
-
 makeGrid(800, 15);
+
+const cellElements = document.querySelectorAll("div");
+let playerTurn = new Boolean(true);
+function selection() {
+    window.addEventListener("click", function(e) {
+        if (e.target.className == "cell" && e.target.id == "notclicked") {
+            e.target.style.backgroundColor = "red";
+            e.target.id = "clicked";
+            console.log(e.target.id);
+        }
+    });
+}
+
+selection();
+computerMove();
+
+//console.log(cellElements);
+
+// let playerTurn = new Boolean(true);
+// const cellElements = document.querySelectorAll("buttons");
+// cellElements.forEach(cell => {
+//     cell.addEventListener('click', selection, { once: true });
+// });
+
+// function selection(e) {
+//     const cell = e.target;
+//     console.log(e);
+// }
+// playGame();
+// playerMove();
+// computerMove();
